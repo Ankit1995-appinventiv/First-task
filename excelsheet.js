3fad4a9b2d798c79913f1a1012cecfd82355eba9
Ankit1995-appinventiv <ankit.nautiyal@appinventiv.com>
2019-08-26 17:13:13 +0530

    file changed

--- excelsheet.js
+++ excelsheet.js
@@ -3,48 +3,87 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 app.use(express.json());
-var csv;
+let csv;
+let db = [];
 let myCars = {};
-app.post("/", (req, res) => {
-  console.log(req.body);
-  myCars = req.body;
-  res.send(req.body);
+let idCount = 1;
 
-  last(myCars);
-});
-
-function last(myCars) {
-  console.log(myCars.name);
-  var fields = [
-    "userDetails.userId",
-    "userDetails.firstName",
-    "userDetails.lastName",
-    "userDetails.address",
-    "userDetails.pincode",
-    "userDetails.mobile"
-  ];
-
-  var parser = new Parser({
-    fields,
-    unwind: [
+app.post(
+  "/",
+  (req, res, next) => {
+    myCars = req.body;
+    myCars.userDetails.userId = idCount;
+    ++idCount;
+    db.push(myCars);
+    res.send(req.body);
+    console.log(db);
+    next();
+    //last(db);
+  },
+  () => {
+    let fields = [
       "userDetails.userId",
       "userDetails.firstName",
       "userDetails.lastName",
       "userDetails.address",
       "userDetails.pincode",
       "userDetails.mobile"
-    ]
-  });
+    ];
+    console.log("has to be exceuted last");
+    let parser = new Parser({
+      fields,
+      unwind: [
+        "userDetails.userId",
+        "userDetails.firstName",
+        "userDetails.lastName",
+        "userDetails.address",
+        "userDetails.pincode",
+        "userDetails.mobile"
+      ]
+    });
 
-  csv = parser.parse(myCars);
-  fs.writeFile("./test-data.csv", csv, err => {
-    if (err) {
-      console.log(err); // Do something to handle the error or just throw it
-      throw new Error(err);
-    }
-    console.log("Success!");
-  });
-}
+    csv = parser.parse(db);
+    fs.writeFile("./test-data.csv", csv, err => {
+      if (err) {
+        console.log(err); // Do something to handle the error or just throw it
+        throw new Error(err);
+      }
+      console.log("Success!");
+    });
+  }
+);
+
+// function last(myCars) {
+//   let fields = [
+//     "userDetails.userId",
+//     "userDetails.firstName",
+//     "userDetails.lastName",
+//     "userDetails.address",
+//     "userDetails.pincode",
+//     "userDetails.mobile"
+//   ];
+
+//   let parser = new Parser({
+//     fields,
+//     unwind: [
+//       "userDetails.userId",
+//       "userDetails.firstName",
+//       "userDetails.lastName",
+//       "userDetails.address",
+//       "userDetails.pincode",
+//       "userDetails.mobile"
+//     ]
+//   });
+
+//   csv = parser.parse(myCars);
+//   fs.writeFile("./test-data.csv", csv, err => {
+//     if (err) {
+//       console.log(err); // Do something to handle the error or just throw it
+//       throw new Error(err);
+//     }
+//     console.log("Success!");
+//   });
+// }
 
 app.listen("7000", () => {
   console.log("started");
